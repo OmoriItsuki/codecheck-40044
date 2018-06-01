3213671d02118179ca81d3926ed7a579b71c952e
kinoko555 <39805387+kinoko555@users.noreply.github.com>
2018-06-01 16:34:27 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,11 @@ int main(int argc, char *argv[])
 -  // start from 1 to ignore script name; argv[0] will be a name of processing file.
 -  for (int i = 1; i < argc; i++) {
 -    printf ("argv[%i]: %s\n", i, argv[i]);
--  }
+ 
 +	string s;
 +	cins >> s;
 +	cout << "Hello " << s << "!" << endl;
+-  }
    return 0;
  }
 }
