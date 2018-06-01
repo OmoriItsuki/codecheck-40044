bf1e5d0c7b407c63963a48d042c77338736e7bad
kinoko555 <39805387+kinoko555@users.noreply.github.com>
2018-06-01 16:35:39 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@ int main(int argc, char *argv[])
 {
  int main(int argc, char *argv[])
  {
--  // start from 1 to ignore script name; argv[0] will be a name of processing file.
--  for (int i = 1; i < argc; i++) {
--    printf ("argv[%i]: %s\n", i, argv[i]);
+ // start from 1 to ignore script name; argv[0] will be a name of processing file.
+  for (int i = 1; i < argc; i++) {
+    printf ("argv[%i]: %s\n", i, argv[i]);
  
-+	string s;
-+	cins >> s;
-+	cout << "Hello " << s << "!" << endl;
--  }
+
+   string s;
+   cins >> s;
+   	cout << "Hello " << s << "!" << endl;
+  }
    return 0;
  }
 }
