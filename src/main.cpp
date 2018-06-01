3a7c6b4eaa796689ae0569c79b2f9580dc5005f8
kinoko555 <39805387+kinoko555@users.noreply.github.com>
2018-06-01 16:34:01 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	string s;
-	cin >> s;
-	cout << "Hello " << s << "!" << endl;
-  return 0;
+ int main(int argc, char *argv[])
+ {
+-  // start from 1 to ignore script name; argv[0] will be a name of processing file.
+-  for (int i = 1; i < argc; i++) {
+-    printf ("argv[%i]: %s\n", i, argv[i]);
+-  }
++	string s;
++	cins >> s;
++	cout << "Hello " << s << "!" << endl;
+   return 0;
+ }
 }
